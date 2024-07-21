803b5973c03ae8f9c284e3f835c6c3a9722ebfd8
San1things <businesslikesan1@gmail.com>
2024-07-21 21:34:09 +0800

    add data on the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Attendee;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1000)->create();
+
+        $this->call(EventSeeder::class);
+        $this->call(AttendeeSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
